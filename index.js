2ee73ad53c6805e76859ca02f031596af260704a
segunNew <eeomo@yahoo.com>
2023-06-21 14:34:44 +0100

    fix

---
+++ index.js
@@ -0,0 +1,12 @@
+$(".burger").click(function(){
+    if ($(".list_item").hasClass("hide")) {
+      $("ul").removeClass("hide");
+    } else {
+      $("ul").addClass("hide");
+    }
+
+    $(".burger").toggleClass("toggle");
+  });
+
+ 
+  
